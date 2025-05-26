de476b8de39c66eb71b6a87bf2eef55f728013df
yassuhacks750 <yassudj8085231272@gmail.com>
2025-05-26 20:43:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,11 +43,6 @@ token_cp = 'your cp token'
 async def account_login(bot: Client, m: Message):
     await m.reply_text(f"**Hello Bruh** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n>>I am TXT file Downloader Bot.\n>>Send me /txt Command And Follow Steps\nIf You Want To Stop Me Just Send /stop to stop me 😎")
 
-@bot.on_message(filters.command("txt"))
-async def restart_handler(_, m):
-    await m.reply_text("🚦**STOPPED**🚦", false)
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
 @bot.on_message(filters.command(["txt"]))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("**Please Send TXT file for download**")
